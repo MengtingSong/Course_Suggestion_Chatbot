e735822c482170572668803dc4f3b2f6040bb9fe
NoSpringNoRain <martina.song@outlook.com>
2018-12-05 14:36:49 -0500

    Layout

--- app/src/main/java/com/example/allen/cours_selection/engelective.java
+++ app/src/main/java/com/example/allen/cours_selection/engelective.java
@@ -6,6 +6,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.ScrollView;
 
 public class engelective extends AppCompatActivity {
 
