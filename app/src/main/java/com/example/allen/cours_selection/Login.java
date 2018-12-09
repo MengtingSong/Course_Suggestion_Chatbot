5070ddf2e42ab72f57469580c16527871dcf33f3
NoSpringNoRain <martina.song@outlook.com>
2018-12-09 04:20:54 -0500

    Merge branch 'master' of https://github.com/zongxinc/Cours_Selection
    # Conflicts:
    #	app/src/main/AndroidManifest.xml
    #	app/src/main/java/com/example/allen/cours_selection/MainActivity.java

--- app/src/main/java/com/example/allen/cours_selection/Login.java
+++ app/src/main/java/com/example/allen/cours_selection/Login.java
@@ -2,21 +2,14 @@ package com.example.allen.cours_selection;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.TextInputEditText;
 import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
 
 public class Login extends AppCompatActivity {
-
-    Button next;
-    TextView info;
-
-    private TextInputLayout id;
-
 
 
     @Override
@@ -25,37 +18,38 @@ public class Login extends AppCompatActivity {
         setContentView(R.layout.activity_login);
 
         Button next = findViewById(R.id.next);
-        EditText id = findViewById(R.id.id);
-        TextView info = findViewById(R.id.info);
+        final TextInputEditText id = findViewById(R.id.id);
+        final TextInputLayout idInput = findViewById(R.id.IDInputLayout);
 
 
         next.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
-                Intent i = new Intent(Login.this, MainActivity.class);
-                startActivity(i);
-                validate(id.getText().toString());
+                if (validate(id)) {
+                    idInput.setError(null);
+                    Intent i = new Intent(Login.this, MainActivity.class);
+                    startActivity(i);
+                }
+                else{
+                    idInput.setError(getString(R.string.error_password));
+                }
+                //validate(id.getText().toString());
             }
         });
 
 
     }
 
-    public boolean validate()
+    public boolean validate(TextInputEditText id)
     {
-        String userid = id.getEditText().getText().toString().trim();
+        String userid = id.getText().toString().trim();
         if(userid.length() == 9)
         {
             Toast.makeText(this,"GREAT! LET'S PROCEED",Toast.LENGTH_SHORT).show();
-            Intent i = new Intent(Login.this, MainActivity.class);
-            startActivity(i);
             return true;
         }
         else
         {
-            info.setError("Please enter a valid BU ID!");
-            next.setEnabled(false);
             return false;
         }
 
